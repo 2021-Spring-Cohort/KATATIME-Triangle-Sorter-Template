0957da121573f4e11ab905585e593c087abbf3b1
Benjamin Williams <33302778+Dean110@users.noreply.github.com>
2021-02-05 15:55:33 -0500

    Update TriangleSorterTest.java

--- src/test/java/triangle_sorter/TriangleSorterTest.java
+++ src/test/java/triangle_sorter/TriangleSorterTest.java
@@ -1,7 +1,7 @@
 package triangle_sorter;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
